b25fd359eafaec43104e6de9ca3b4287e31b8e3a
Nina Baculinao <nb2406@columbia.edu>
2015-09-22 17:52:05 -0400

    Set before_filter to require_user for all controllers. Add current_user and require_user method in application_controller.rb.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Filters are inherited so a controller inherits its parent's filters
+  # All controllers in the app inherit from ApplicationController
+  before_filter :require_user
+
+  def current_user
+    if @current_user.nil?
+      @current_user = User.find(session[:user_id]["$oid"]) if session[:user_id]
+    end
+  end
+  helper_method :current_user
+
+  def require_user
+    if current_user
+      return true
+    end
+    redirect_to login_path
+  end
+
 end
